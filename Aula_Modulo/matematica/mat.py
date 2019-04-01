2cfaa45f4703f7599da2c9a5d5a782c4a592aa17
Felipe <felipeiasi@gmail.com>
2019-04-01 20:01:57 -0300

    Aula Modulo - Docstring e comentarios

--- Aula_Modulo/matematica/mat.py
+++ Aula_Modulo/matematica/mat.py
@@ -1,7 +1,15 @@
+"Módulo que contem operações matemáticas"
+
 def soma(parcela, parcela2):
+    """Essa função calcula a soma de duas parcelas
+
+    :param parcela: numero
+    :param parcela2: numero
+    :return: numero
+    """
     return parcela + parcela2
 
 
 if __name__ == '__main__':
-    print(__name__)
-    print(soma(2, 4))
+    # Testamdo a função soma
+    print(soma(2, 4)) #Imprimindo
