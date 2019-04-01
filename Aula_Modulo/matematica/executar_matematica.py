70c3c4da8dc40e01392f5bfcd4e01d199d5954cf
Felipe <felipeiasi@gmail.com>
2019-04-01 19:55:10 -0300

    Aula Modulo

---
+++ Aula_Modulo/matematica/executar_matematica.py
@@ -0,0 +1,3 @@
+import matematica.mat
+
+print(matematica.mat.soma(4, 5))
---
+++ Aula_Modulo/matematica/mat.py
@@ -0,0 +1,7 @@
+def soma(parcela, parcela2):
+    return parcela + parcela2
+
+
+if __name__ == '__main__':
+    print(__name__)
+    print(soma(2, 4))
